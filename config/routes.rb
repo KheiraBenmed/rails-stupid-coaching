19304d2ab2ceff9b9a4dfcc65cdedd1d6550bd1b
Kheira Benmeridja <kheira.benmeridja@gmail.com>
2017-05-15 12:51:01 +0200

    new wstupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'coaching/ask' => 'coaching#ask'
+  get 'coaching/answer' => 'coaching#answer'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
